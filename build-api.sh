aadf7034e7ac1af4e7e08aa004e90316a00a9da6
marcj <marc@marcjschmidt.de>
2021-06-21 20:41:15 +0200

    fix build

--- build-api.sh
+++ build-api.sh
@@ -10,6 +10,14 @@ else
   cd /tmp/deepkit-framework-docs;
 fi
 npm i;
+npm run bootstrap -- --scope @deepkit/core --scope @deepkit/core-rxjs --scope @deepkit/app --scope @deepkit/broker \
+  --scope @deepkit/bson --scope @deepkit/crypto --scope @deepkit/desktop-ui --scope @deepkit/event \
+  --scope @deepkit/framework --scope @deepkit/http --scope @deepkit/injector --scope @deepkit/logger \
+  --scope @deepkit/mongo --scope @deepkit/mysql --scope @deepkit/orm --scope @deepkit/orm-integration --scope @deepkit/postgres \
+  --scope @deepkit/rpc --scope @deepkit/rpc-tcp --scope @deepkit/sql --scope @deepkit/sqlite \
+  --scope @deepkit/stopwatch --scope @deepkit/template --scope @deepkit/topsort --scope @deepkit/type \
+  --scope @deepkit/type-angular --scope @deepkit/workflow
+npm run link;
 npm run tsc;
 NODE_OPTIONS=--max_old_space_size=8096 ./node_modules/.bin/typedoc --packages 'packages/*';
 cd -;
