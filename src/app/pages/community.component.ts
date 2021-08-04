6033321ecaf009979fb7c2f79b909e7abc1e3b01
marcj <marc@marcjschmidt.de>
2021-08-04 15:45:19 +0200

    community

--- src/app/pages/community.component.ts
+++ src/app/pages/community.component.ts
@@ -47,7 +47,8 @@ import { Component } from '@angular/core';
                 <h3>Twitter</h3>
             </a>
 
-<!--            <a class="card" target="_blank" href="https://www.youtube.com/channel/UCj5Y7-zUm-xVmF49LhuU7Kg">-->
+
+            <!--            <a class="card" target="_blank" href="https://www.youtube.com/channel/UCj5Y7-zUm-xVmF49LhuU7Kg">-->
 <!--                <div class="icon-circle">-->
 <!--                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor"-->
 <!--                         stroke-width="2" stroke-linecap="round" stroke-linejoin="round">-->
